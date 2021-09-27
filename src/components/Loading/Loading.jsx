311f7aa7763ae5f2260d017be71332ec7a6ec930
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-09-27 23:18:13 +0400

    feat(Loading): Added load spinner and style

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./Loading.module.css";
+
+const Loading = () => (
+  <div className={classes.ldsSpinner}>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
+
+export default Loading;
