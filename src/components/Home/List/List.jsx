8565a9228fc01451b759d24478a1844914ac9b6b
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-09-30 16:10:20 +0400

    feat(List): Added tags and women filteres

--- src/components/Home/List/List.jsx
+++ src/components/Home/List/List.jsx
@@ -1,10 +1,10 @@
-import Loading from "../../Loading/Loading";
+import { useEffect, useState } from "react";
 import cn from "classnames";
+import Loading from "../../Loading/Loading";
+import Card from "./Card/Card";
+import { getCategories, getTags } from "../../../services/service";
 
 import classes from "./List.module.css";
-import Card from "./Card/Card";
-import { useEffect, useState } from "react";
-import { getCategories, getTags } from "../../../services/service";
 
 function List({
   error,
@@ -17,6 +17,8 @@ function List({
 }) {
   const [card, setCard] = useState(false);
   const [filteredItem, setFilteredItem] = useState([]);
+  const [filteredListItem, setFilteredListItem] = useState();
+  const [filteredTags, setFilteredTags] = useState();
   const [women, setWomen] = useState([]);
   const [tag, setTag] = useState([]);
 
@@ -51,92 +53,119 @@ function List({
   };
 
   const handleWomenClick = (e) => {
-    console.log(e.target);
+    const listItem = e.target.innerText;
+    const listFilterItem = categories.filter(
+      (item) => item.category === listItem
+    );
+    setFilteredListItem(listFilterItem);
+    setFilteredTags("");
   };
+
+  const handleAllClick = () => {
+    setFilteredListItem("");
+    setFilteredTags("");
+  };
+
+  const handleTagsClick = (e) => {
+    const listTags = e.target.innerText;
+    const listFilterTags = categories.filter((item) => item.tag === listTags);
+    setFilteredTags(listFilterTags);
+    setFilteredListItem("");
+  };
+
+  const showedList = filteredListItem
+    ? filteredListItem
+    : filteredTags
+    ? filteredTags
+    : categories;
 
   return (
     <div
-      className={cn([classes.list], {
+      className={cn([classes.backgroundContainerList], {
         [classes.hide]: !list,
       })}
     >
-      <div className={classes.title}>
-        <h1> Shoes Shopping </h1>
-        {loading && <p> Loading... </p>}
-        {error && <p> Error... </p>}
-        <div className={classes.alfa}>
-          {women &&
-            women.map((el) => {
+      <div className={classes.list}>
+        <div className={classes.title}>
+          <h1 onClick={handleAllClick} className={classes.allList}>
+            Shoes Shopping
+          </h1>
+          {loading && <b> Loading... </b>}
+          {error && <b> Error... </b>}
+          <div className={classes.alfa}>
+            {women &&
+              women.map((el) => {
+                return (
+                  <div className={classes.infoPage} key={Math.random()}>
+                    <p onClick={handleWomenClick}>{el}</p>
+                  </div>
+                );
+              })}
+          </div>
+          {loading && <b> Loading... </b>}
+          {error && <b> Error... </b>}
+          <div className={classes.gamma}>
+            {tag &&
+              tag.map((elem) => {
+                return (
+                  <div className={classes.infoPage} key={Math.random()}>
+                    <i onClick={handleTagsClick}>{elem}</i>
+                  </div>
+                );
+              })}
+          </div>
+          <button onClick={handleOnClick} className={classes.close}>
+            X
+          </button>
+        </div>
+        {loading && <Loading />}
+        {error && <b>ERROR ...</b>}
+        <div className={classes.wrapperList}>
+          {showedList &&
+            showedList.map((e) => {
               return (
-                <div className={classes.infoPage} key={Math.random()}>
-                  <p onClick={handleWomenClick}>{el}</p>
+                <div
+                  onClick={() => handleCardClick(e.id)}
+                  key={e.id}
+                  className={classes.listContainer}
+                >
+                  <div>
+                    <h2 className={classes.categoryStyle}>{e.category}</h2>
+                    <h3>{e.name}</h3>
+                    <h4>{e.long_description}</h4>
+                  </div>
+                  <div className={classes.imageBlock}>
+                    <img
+                      className={classes.infoListImage}
+                      src={e.images[0]}
+                      alt="Sport shoes"
+                    />
+                    <img
+                      className={classes.infoListImage}
+                      src={e.images[1] ? e.images[1] : e.images[0]}
+                      alt="Sport shoes"
+                    />
+                  </div>
+                  <h5>{e.short_description}</h5>
+                  <div className={classes.spanBlockList}>
+                    <span>{e.price}$</span>
+                    <span>quantity {e.quantity}</span>
+                    <span>{e.status}</span>
+                  </div>
                 </div>
               );
             })}
         </div>
-        {loading && <p> Loading... </p>}
-        {error && <p> Error... </p>}
-        <div className={classes.gamma}>
-          {tag &&
-            tag.map((elem) => {
-              return (
-                <div className={classes.infoPage} key={Math.random()}>
-                  <i>{elem}</i>
-                </div>
-              );
-            })}
-        </div>
-        <button onClick={handleOnClick} className={classes.close}>
-          X
-        </button>
+        {card && (
+          <Card
+            handleCardClick={handleCardClick}
+            card={card}
+            filteredItem={filteredItem}
+            error={error}
+            loading={loading}
+          />
+        )}
       </div>
-      {loading && <Loading />}
-      {error && <p>ERROR ...</p>}
-      <div className={classes.wrapperList}>
-        {categories &&
-          categories.map((e) => {
-            return (
-              <div
-                onClick={() => handleCardClick(e.id)}
-                key={e.id}
-                className={classes.listContainer}
-              >
-                <div>
-                  <h2 className={classes.categoryStyle}>{e.category}</h2>
-                  <h3>{e.name}</h3>
-                  <h4>{e.long_description}</h4>
-                </div>
-                <div className={classes.imageBlock}>
-                  <img
-                    className={classes.infoListImage}
-                    src={e.images[0]}
-                    alt="Sport shoes"
-                  />
-                  <img
-                    className={classes.infoListImage}
-                    src={e.images[1] ? e.images[1] : e.images[0]}
-                    alt="Sport shoes"
-                  />
-                </div>
-                <h5>{e.short_description}</h5>
-                <div className={classes.spanBlockList}>
-                  <span>{e.price}$</span>
-                  <span>quantity {e.quantity}</span>
-                  <span>{e.status}</span>
-                </div>
-              </div>
-            );
-          })}
-      </div>
-      {card && (
-        <Card
-          handleCardClick={handleCardClick}
-          card={card}
-          filteredItem={filteredItem}
-          error={error}
-          loading={loading}
-        />
-      )}
     </div>
   );
 }
